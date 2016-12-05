5b8074ad01d6fd7d3fc146a6d7df1d606f78b24d
vera_john <vera_john@MacBook-Air.local>
2016-12-05 15:11:56 -0600

    trading games v0.1

---
+++ app/src/main/java/nowoscmexico/com/tradinggames_1/MainActivity.java
@@ -0,0 +1,24 @@
+package nowoscmexico.com.tradinggames_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import nowoscmexico.com.tradinggames_1.tuto.TutorialActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showTuto(View v){
+
+        Intent i = new Intent(this, TutorialActivity.class);
+        startActivity(i);
+
+    }
+}
